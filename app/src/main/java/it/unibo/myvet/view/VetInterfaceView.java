78594c86aa4bbe6caf872291580d80a1562d34fa
Carbocode <noreply@prisco.me>
2024-08-25 17:31:37 +0200

    fixed opening vet view

--- app/src/main/java/it/unibo/myvet/view/VetInterfaceView.java
+++ app/src/main/java/it/unibo/myvet/view/VetInterfaceView.java
@@ -8,13 +8,18 @@ import it.unibo.myvet.controller.AppointmentListController;
 import it.unibo.myvet.dao.AppointmentDAO;
 import it.unibo.myvet.model.*;
 
-public class VetInterfaceView extends JPanel {
-    Vet vet;
-    AppointmentDAO appointmentDAO = new AppointmentDAO();
+public class VetInterfaceView extends JFrame {
+    private Vet vet;
+    private AppointmentDAO appointmentDAO = new AppointmentDAO();
 
     public VetInterfaceView(Vet vet) {
         this.vet = vet;
 
+        // Configurazione del JFrame
+        setTitle("Vet Interface");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(1200, 600);
+        setLocationRelativeTo(null);
         setLayout(new GridLayout(1, 2));
 
         List<Appointment> appointments = appointmentDAO.findByVetId(this.vet.getVetId());
@@ -44,29 +49,23 @@ public class VetInterfaceView extends JPanel {
         upcomingPanel.add(upcomingLabel, BorderLayout.NORTH);
         upcomingPanel.add(upcomingListView, BorderLayout.CENTER);
 
-        // Aggiungi entrambi i pannelli al pannello principale
+        // Aggiungi entrambi i pannelli al JFrame
         add(requestsPanel);
         add(upcomingPanel);
+
+        // Mostra la finestra
+        setVisible(true);
     }
 
     // Metodo main per testare la GUI
     public static void main(String[] args) {
         // Creazione di dati fittizi per testare la view
-
         Specialization specialization = new Specialization("General");
 
         // Creazione di alcuni veterinari
         Vet vet1 = new Vet("PRSVCN02P21D704E", "vetpassword1", "Dr.", "Jones", "111222333", 2, specialization);
 
-        // Creazione del JFrame e aggiunta del pannello principale
-        JFrame frame = new JFrame("Vet Interface");
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(1200, 600);
-        frame.setLocationRelativeTo(null);
-
-        VetInterfaceView vetInterfaceView = new VetInterfaceView(vet1);
-        frame.add(vetInterfaceView);
-
-        frame.setVisible(true);
+        // Creazione e visualizzazione della finestra principale
+        new VetInterfaceView(vet1);
     }
 }
